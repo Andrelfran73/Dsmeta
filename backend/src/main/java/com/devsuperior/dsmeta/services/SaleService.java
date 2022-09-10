d14c7c92f59c1093e8ba6daa0232c228087d8739
Andrelfran73 <andrelfran@hotmail.com>
2022-09-10 18:24:54 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -3,10 +3,12 @@ package com.devsuperior.dsmeta.services;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
@@ -19,7 +21,7 @@ public class SaleService {
 	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
 		
-		LocalDate min = minDate.equals("") ? today .minusDays(365): LocalDate.parse(minDate);
+		LocalDate min = minDate.equals("") ? today.minusDays(365): LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
 		return repository.findSales(min, max, pageable);
 	}
